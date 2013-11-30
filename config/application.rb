2e27cd223af0f329c48d862aa7c5b5504698d58b
Michael Rokosh <mike.rokosh@gmail.com>
2013-11-30 14:14:54 -0500

    sljfnv

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 module Politreview
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
-    require 'lib/GeoIP.dat'
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
