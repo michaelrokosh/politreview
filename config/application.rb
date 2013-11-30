bff57124512e2c79832e788e636a9e5ef9e1337c
Michael Rokosh <mike.rokosh@gmail.com>
2013-11-30 14:25:17 -0500

    sljfnv1

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+config.autoload_paths += %W(#{config.root}/lib)
+require 'lib/geoip.dat'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Politreview
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
+    
   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
