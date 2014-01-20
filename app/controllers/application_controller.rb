7ae97273121dcb4739bc05cdff3cf4698475092d
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-20 08:19:43 -0500

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_recent_news
   protect_from_forgery with: :exception
+
+  def set_recent_news
+  	@recent_news = News.order("created_at desc").limit(5).offset(0)
+  end
 end
