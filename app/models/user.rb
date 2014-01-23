364110127bd539f0e1bd94262b9e8410305b6a2a
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-23 16:59:01 -0500

    keywords

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,12 @@ class User < ActiveRecord::Base
                       length: { minimum: 3, maximum: 15, message: "должно быть в пределе 3-15 символов" },
                       exclusion: { in: %w(pages page tags tag search results recent top subject subjects categories category books book user users sign_in sign_out registration pro about contact adv advertise store shop),
                       message: "занят" }
+
+  def voted_for?(voteable)
+    votes.where(voteable_id: voteable).first.present?
+  end
+
+  def positive_vote?(voteable)
+    votes.where(voteable_id: voteable).first.rate >= 1
+  end
 end
