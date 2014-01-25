121fd48103595863a4b1f4190e6a717d5ec3a374
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-25 13:32:15 -0500

    keywords

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ActiveRecord::Base
   has_many :votes
   VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\Z/
   validates :username, presence: true, 
-                      format: { with: VALID_USERNAME_REGEX, message: "должно состоять только из символов и цифр" },
                       uniqueness: { case_sensitive: false, message: "занато" },
                       length: { minimum: 3, maximum: 15, message: "должно быть в пределе 3-15 символов" },
                       exclusion: { in: %w(pages page tags tag search results recent top subject subjects categories category books book user users sign_in sign_out registration pro about contact adv advertise store shop),
