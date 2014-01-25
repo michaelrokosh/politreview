ccdd397f64f6eb1c90faa523555de9cdcade41bf
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-25 13:29:10 -0500

    keywords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125180723) do
+ActiveRecord::Schema.define(version: 20140125182844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20140125180723) do
     t.boolean  "admin",                  default: false
     t.string   "provider"
     t.string   "avatar_url",             default: "/avatar.png"
+    t.string   "url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
