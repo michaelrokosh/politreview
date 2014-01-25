5f2a90992d7bd5f07bb51e826559cdcd6dab33a7
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-25 05:32:08 -0500

    Set values

---
+++ db/migrate/20140125102428_change_politics_set_rating.rb
@@ -0,0 +1,7 @@
+class ChangePoliticsSetRating < ActiveRecord::Migration
+  def change
+  	Politic.all.each do |p|
+  		p.update_attributes(rating: 0.0, votes_counter: 0.0)
+  	end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124230558) do
+ActiveRecord::Schema.define(version: 20140125102428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
