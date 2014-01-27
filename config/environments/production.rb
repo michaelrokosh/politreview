a6664d497b03b03929788f0967a7bc2b74b7c8db
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-27 12:58:53 -0500

    keywords

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Politreview::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += %w( editor.js )
   # Code is not reloaded between requests.
   config.cache_classes = true
   
